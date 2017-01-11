8b256c9bbc338274933a23946c1cab7c41f7ffd0
Catherine Gracey <catherine.gracey@gmail.com>
2017-01-12 08:30:33 +1100

    Outline of tests needed

---
+++ Ruby/robot_test.rb
@@ -0,0 +1,11 @@
+# Test placing the robot in a valid location facing in all 4 directions
+# Test placing the robot in an invalid direction
+# Test placing the robot too high on the board
+# Test placing the robot too low on the board
+# Test placing the robot too far right on the board
+# Test placing the robot too far left on the board
+# Test moving the robot one valid tile in all 4 directions
+# Test running the robot off the board in all 4 directions
+# Test turning the robot left in all 4 directions
+# Test turning the robot right in all 4 directions
+# Test robot output
